a0383500c4b0abe0b4413a2f873f35c52eada4c9
thecwadley <51142831+thecwadley@users.noreply.github.com>
2020-09-19 22:44:43 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,28 @@ event.waitUntil(
       return cache.addAll(FILES_TO_CACHE);
     })
 );
+
+event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+);
+
+if (event.request.mode !== 'navigate') {
+  // Not a page navigation, bail.
+  return;
+}
+event.respondWith(
+    fetch(event.request)
+        .catch(() => {
+          return caches.open(CACHE_NAME)
+              .then((cache) => {
+                return cache.match('offline.html');
+              });
+        })
+);
