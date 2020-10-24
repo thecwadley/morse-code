2c7d21195453f90ca78f84efed8188db0e00b830
thecwadley <51142831+thecwadley@users.noreply.github.com>
2020-10-24 15:16:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Speech from "https://github.com/tom-s/speak-tts/blob/master/lib/speak-tts.js";
+import Speech from "../morse-code/speak-tts-master/src/speak-tts.js";
 let speech;
 
 var right = new Audio('https://raw.githubusercontent.com/thecwadley/morse-code/master/audio/right.mp3');
